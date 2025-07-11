6931b0b6304200bc61c8bef16a77004297eb9f4c
sejun-Park3312 <dlrhddlfvkf21207@pusan.ac.kr>
2025-07-11 11:12:18 +0900

    .

--- RobotSimulation.py
+++ RobotSimulation.py
@@ -3,6 +3,7 @@ import pybullet_data
 import time
 import math as m
 import numpy as np
+import threading
 
 class RobotSimulaition:
     def __init__(self):
@@ -11,14 +12,16 @@ class RobotSimulaition:
         self.physicsClient = p.connect(p.GUI)
         self.robotId = p.loadURDF(r"C:\Users\qkrtp.DESKTOP-60UHPRL\PycharmProjects\DoosanRobotics\robot.urdf",useFixedBase=True)
         self.HomePosition = np.array([0, 0, m.pi/2, 0, m.pi/2, 0])
-        self.Send_q(self.HomePosition)
         self.T_EE = None
+        self.q = self.HomePosition
+        self.Get_Config()
         self.Get_T_EE()
+        self.lock = threading.Lock()
 
 
-    def Send_q(self, q):
+    def Get_Config(self):
         for j in range(6):
-            p.resetJointState(self.robotId, j, q[j])
+            p.resetJointState(self.robotId, j, self.q[j])
 
 
     def Get_T_EE(self):
@@ -34,4 +37,15 @@ class RobotSimulaition:
         self.T_EE = T_EE
 
 
+    def StartSimulation(self):
+        while self.Running:
+
+            with self.lock:
+                self.Get_Config()
+            p.stepSimulation()
+            time.sleep(1. / 240.)
+
+        self.EndSimulation()
+
     def EndSimulation(self):
+        p.disconnect()
